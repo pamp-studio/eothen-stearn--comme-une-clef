48f2f682aa73f1f730f6f1c3265bc99287d35e7f
pamp the horn <pampthehorn@gmail.com>
2020-09-23 11:29:24 +0100

    init

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+var cacheHistory = ['v0'];
+var cacheName = 'v0.01';
+var urlsToCache = [
+  'index.html',
+  'style.css',
+  'js/script.js',
+  'media/audio/Eothen-Stearn-Comme-Une-Clef.mp3'
+]
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) { 
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(cacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheHistory.indexOf(cacheName) !== -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
