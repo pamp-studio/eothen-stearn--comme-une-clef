3bcfaf59cc202dc14b285c8f2d3cb02d7c5865c9
Pamp <pamp@Pamps-MacBook-Pro.local>
2020-09-24 14:54:10 +0100

    adding copy and making credits section

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var cacheHistory = ['v0.01','v0.02','v0.03','v0.04','v0.05','v0.06','v0.07','v0.08','v0.09','v0.10','v0.11'];
-var cacheName = 'v0.00';
+var cacheHistory = ['v0.00','v0.01','v0.02','v0.03','v0.04','v0.05','v0.06','v0.07','v0.08','v0.09','v0.10','v0.11'];
+var cacheName = 'v0.001';
 var urlsToCache = [
   'index.html',
   'style.css',
